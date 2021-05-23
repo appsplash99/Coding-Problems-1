64dabc41a9e8c09a10c1f3c201d3fe2d44868069
Nitesh.thapliyal <53345517+Nitesh07thapliyal@users.noreply.github.com>
2021-05-23 18:25:17 +0530

    Day 1 python basic

---
+++ Python/basic.py
@@ -0,0 +1,16 @@
+x = 5 #We don't need to mention the datatype in python it dynamically detect's it
+print (x)
+print(type(x)) #type show the type of data type
+
+y = "hi" # Everything inside string is string
+print (y)
+print(type(y))
+
+z = True # Boolean value i.e true and false
+print(z)
+print(type(z))
+
+x =6 #here x is variable and 6 is data stored in x
+
+#In python if we store data it takes 4 byte space in RAM
+# python3 is an interterpreter that use to execute the code
